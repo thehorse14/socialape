0afae1333d0095fdcad3cf482bb68799074280c2
Ooi Tiat Han <ooi.tiathan@student.usm.my>
2020-09-07 02:05:41 +0800

    Drop useless config

--- util/admin.js
+++ util/admin.js
@@ -1,12 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../service-account");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://social-5f7d3.firebaseio.com",
-    storageBucket: "social-5f7d3.appspot.com",
-});
+admin.initializeApp();
 
 const db = admin.firestore();
 
-module.exports = {admin, db};
+module.exports = { admin, db };
