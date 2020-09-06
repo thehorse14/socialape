310a84b4b96cbe5a4a21963178c70a0d55362742
Ooi Tiat Han <ooi.tiathan@student.usm.my>
2020-09-06 13:20:38 +0800

    Add get user credentials api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const functions = require("firebase-functions");
 const app = require("express")();
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signUp, login, uploadImage, addUserDetails } = require("./handlers/users");
+const { signUp, login, uploadImage, addUserDetails, getAuthenticatedUser } = require("./handlers/users");
 
 const FBAuth = require("./util/fbAuth");
 
@@ -20,6 +20,7 @@ app.post("/signup", signUp);
 app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
 app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 
 
