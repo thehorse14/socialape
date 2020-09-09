7e9f2dd6874c679b1337b9e59948716d6d567cd2
Ooi Tiat Han <ooi.tiathan@student.usm.my>
2020-09-10 00:18:00 +0800

    Add notification in get user details

--- handlers/users.js
+++ handlers/users.js
@@ -119,6 +119,22 @@ exports.getAuthenticatedUser = (req, res) => {
             data.forEach(doc => {
                 userData.likes.push(doc.data());
             })
+            return db.collection("notification").where("recipient", "==", req.user.handle)
+                .orderBy("createdAt", "desc").limit(10).get();
+        })
+        .then(data => {
+            userData.notification = [];
+            data.forEach(doc => {
+                userData.notification.push({
+                    recipient: doc.data().recipient,
+                    sender: doc.data().sender,
+                    createdAt: doc.data().createdAt,
+                    screamId: doc.data().screamId,
+                    type: doc.data().type,
+                    read: doc.data().read,
+                    notificationId: doc.id
+                });
+            });
             return res.json(userData);
         })
         .catch(err => {
