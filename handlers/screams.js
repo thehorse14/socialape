2ef49bacefc223cf86ac9217d7297ff1af9580ea
Ooi Tiat Han <ooi.tiathan@student.usm.my>
2020-09-13 17:39:18 +0800

    Fix error messages and document attributes

--- handlers/screams.js
+++ handlers/screams.js
@@ -11,7 +11,10 @@ exports.getAllScreams = (req, res) => {
                     screamId: doc.id,
                     body: doc.data().body,
                     userHandle: doc.data().userHandle,
-                    createdAt: doc.data().createdAt
+                    createdAt: doc.data().createdAt,
+                    commentCount: doc.data().commentCount,
+                    likeCount: doc.data().likeCount,
+                    userImage: doc.data().userImage
                 });
             });
 
@@ -71,7 +74,7 @@ exports.getScream = (req, res ) => {
 
 exports.commentOnScream = (req, res) => {
     if(req.body.body.trim() === "")
-        return res.status(400).json({ error: "Must not be empty"});
+        return res.status(400).json({ comment: "Must not be empty"});
     
     const newComment = {
         body: req.body.body,
--- handlers/users.js
+++ handlers/users.js
@@ -58,7 +58,7 @@ exports.signUp = (req, res) => {
             if (err.code === "auth/email-already-in-use") {
                 return res.status(400).json({ email: "Email is already in use" });
             } else {
-                return res.status(500).json({ error: err.code });
+                return res.status(500).json({ general: "Something went wrong, please try again" });
             }
         });
 }
@@ -82,10 +82,9 @@ exports.login = (req, res) => {
         })
         .catch(err => {
             console.error(err);
-            if (err.code === "auth/wrong-password") {
-                return res.status(403).json({ general: "Wrong credentials, please try again." });
-            }
-            return res.status(500).json({ error: err.code });
+            // auth/wrong-password
+            // auth/user-not-found
+            return res.status(403).json({ general: "Wrong credentials, please try again." });
         })
 }
 
